16a7dc2cb198961ab43dec826f514c74b3e8a4e5
suucong <suucong2@swu.ac.kr>
2025-10-16 15:03:46 +0900

    feat: 커스텀 구분자 추출 기능 구현
    - parseDelimiters 함수 추가
    - 정규식 사용하여 추출

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요. \n"
-    );
+    const input = (
+      await Console.readLineAsync("덧셈할 문자열을 입력해 주세요. \n")
+    ).trim();
 
     const result = calculate(input);
 
@@ -17,7 +17,24 @@ function calculate(inputString) {
     return 0;
   }
 
-  return inputString;
+  const { delimiters, numberString } = parseDelimiters(inputString);
+
+  return inputString; // 임시 반환
+}
+
+function parseDelimiters(inputString) {
+  const delimiters = [",", ":"];
+  let numberString = inputString;
+
+  if (inputString.startsWith("//")) {
+    const parts = inputString.split(/\n|\\n/);
+    const customDelimiter = parts[0].substring(2);
+
+    delimiters.push(customDelimiter);
+    numberString = parts[1];
+  }
+
+  return { delimiters, numberString };
 }
 
 export default App;
